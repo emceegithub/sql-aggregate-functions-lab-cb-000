e1bf72d4d841c19fc8221750f80b64ec8e69975d
Mark Cruz <markchriscruz@yahoo.com>
2018-05-08 21:44:59 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "select avg(gpa) from students"
+  "select avg(gpa) from students;"
 end
 
 def total_tardies_for_all_students
-  "select sum(tardies) from students"
+  "select sum(tardies) from student;s"
 end
 
 def average_gpa_for_9th_grade
